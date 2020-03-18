cd3fbe234fb1590d43f625ed554529c32b4428c0
pepsiftd <pepsiftd@gmail.com>
2020-03-18 23:50:51 +0300

    Добавляет функцию загрузки данных на сервер

--- js/ajax.js
+++ js/ajax.js
@@ -1,7 +1,8 @@
 'use strict';
 
 (function () {
-  var SERVER = 'https://js.dump.academy/keksobooking/data';
+  var DATA_SERVER = 'https://js.dump.academy/keksobooking/data';
+  var FORM_SERVER = 'https://js.dump.academy/keksobooking';
   var StatusCode = {
     OK: 200
   };
@@ -31,11 +32,40 @@
 
     xhr.timeout = TIMEOUT_IN_MS;
 
-    xhr.open('GET', SERVER);
+    xhr.open('GET', DATA_SERVER);
     xhr.send();
   };
 
+  var upload = function (data, successHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    var loadHandler = function () {
+      if (xhr.status === StatusCode.OK) {
+        successHandler(xhr.response);
+      } else {
+        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    };
+
+    xhr.addEventListener('load', loadHandler);
+
+    xhr.addEventListener('error', function () {
+      errorHandler('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      errorHandler('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('POST', FORM_SERVER);
+    xhr.send(data);
+  };
+
   window.ajax = {
-    load: load
+    load: load,
+    upload: upload
   };
 })();
